61f6a059e2a67f2aa80b961b76cc2ae709d76cc7
NicholasBoss <NicholasBoss0914@gmail.com>
2025-04-02 16:25:39 -0600

    added try catch block to project cards

--- public/js/projectcards.js
+++ public/js/projectcards.js
@@ -3,15 +3,19 @@ const projectCards = document.querySelector('.project-cards')
 
 function displayCards(data) {
     let projectsData = data.projects;
-    projectsData.forEach(project => {
-        const projectItem = document.createElement('div');
-        projectItem.setAttribute('class', 'project-card')
-        projectItem.innerHTML = `
-            <h4>${project.name}</h4>
-            <p>${project.description}</p>
-            `;
-            projectCards.appendChild(projectItem);
-        });
+    try {
+        projectsData.forEach(project => {
+            const projectItem = document.createElement('div');
+            projectItem.setAttribute('class', 'project-card')
+            projectItem.innerHTML = `
+                <h4>${project.name}</h4>
+                <p>${project.description}</p>
+                `;
+                projectCards.appendChild(projectItem);
+            });
+    } catch (error) {
+        console.error('Error displaying project cards:', error);
+    }
     }
     // <a href="${project.link}">Learn More</a>
 
